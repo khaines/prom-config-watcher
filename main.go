c730395eec7bbf6b659897b117b89c51582ba6c3
Ken Haines <ken@kenhaines.net>
2018-02-04 15:02:55 -0800

    - added env variable expansion - batching multiple change events under 1 processing pass based on a delay timer - processing directories as a set of files

--- main.go
+++ main.go
@@ -22,9 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
 
-
 package main
-
 
 import (
 	log "github.com/sirupsen/logrus"
@@ -33,18 +31,22 @@ import (
 	"os"
 
 	"time"
+
+	"os/signal"
+	"syscall"
+	"io/ioutil"
+	"path"
 )
 
-
 var (
-	watchedPath =  flag.String("watch-path","/config","Path to be watched (default /config)")
-	expandVars = flag.Bool("expand-vars",true,"Expand $env variables found in files (default true).")
-	copyFiles = flag.Bool("copy-files",true,"Copy config files to destination (default true)")
-	targetPath = flag.String("target-path","/processed-config","Path to copy processed files to (default /processed-config)")
-	prometheusUrl = flag.String("prometheus-url","http://localhost:9090/-/reload", "Url to send a POST to prometheus for it to reload its config. (default http://localhost:9090/-/reload)")
-	debugLogs = flag.Bool("debug",false,"Enable debug log output")
-	)
-
+	watchedPath      = flag.String("watch-path", "/config", "Path to be watched (default /config)")
+	expandVars       = flag.Bool("expand-vars", true, "Expand $env variables found in files (default true).")
+	copyFiles        = flag.Bool("copy-files", true, "Copy config files to destination (default true)")
+	targetPath       = flag.String("target-path", "/processed-config", "Path to copy processed files to (default /processed-config)")
+	prometheusUrl    = flag.String("prometheus-url", "http://localhost:9090/-/reload", "Url to send a POST to prometheus for it to reload its config. (default http://localhost:9090/-/reload)")
+	processDelayTime = flag.Duration("process-delay-time", 5*time.Second, "time to wait after a detected change to process files. This allows capturing multiple close timed changes in a single update.")
+	debugLogs        = flag.Bool("debug", false, "Enable debug log output")
+)
 
 func main() {
 	log.SetLevel(log.InfoLevel)
@@ -52,67 +54,134 @@ func main() {
 	log.Info("Github: https://github.com/khaines/prom-config-watcher")
 	flag.Parse()
 
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	lastConfigProcess := time.Time{}
+	// initializing config change to now will trigger an initial run to process the config files
+	lastConfigChange := time.Now()
+	delayTimer := time.NewTimer(0)
+
+	fileChangeTime, err := startWatchingPath(*watchedPath)
+	if err != nil {
+		log.Fatalf("Failed to start watching path %v, exiting")
+		os.Exit(1)
+		return
+	}
+
+	for {
+		select {
+		case <-sigs:
+			log.Infof("Received SIGINT or SIGTERM. Shutting down")
+			os.Exit(0)
+			return
+		case lastConfigChange = <-fileChangeTime:
+			// reset the delay timer in case other changes are triggered rapidly
+			delayTimer.Reset(*processDelayTime)
+
+		case <-delayTimer.C:
+			// process delay timer has tripped, process the config files.
+			if lastConfigProcess.Before(lastConfigChange) {
+				// process
+				processConfigChanges(*watchedPath,*targetPath)
+				lastConfigProcess = time.Now()
+			}
+
+		}
+
+	}
 
 }
 
+func processConfigChanges(srcPath string,dstPath string) {
+	log.Debugf("Processing changes for %v",srcPath)
+	// if we are in a folder, process the files within
+	stat, err := os.Stat(srcPath)
+	if err != nil{
+		log.Errorf("Error processing changes in %v: %v", srcPath,err)
+		return
+	}
 
-func startWatchingPath(path string) (chan time.Time,error) {
+	if stat.IsDir(){
+		files, err := ioutil.ReadDir(srcPath)
+		if err != nil{
+			log.Errorf("Failed to list files in %v: %v", srcPath,err)
+			return
+		}
 
-	log.Debugf("Creating watcher for path %v",path)
+		for _,fileName := range files {
+			processConfigChanges(path.Join(srcPath,fileName.Name()),dstPath)
+		}
+	}else{
+		processFile(srcPath,dstPath)
+	}
+}
+
+func processFile(filePath string,destFolder string) {
+
+	// read in the file
+	contents,err := ioutil.ReadFile(filePath)
+	if err !=nil {
+		log.Errorf("Error reading %v: %v",filePath,err)
+	}
+	// expand any environmenal vars present
+	updatedContent := os.ExpandEnv(string(contents))
+
+	// write updated content to the destination folder
+	_,fileName := path.Split(filePath)
+	targetFile := path.Join(destFolder,fileName)
+	log.Debugf("writing updated content to %v",targetFile)
+	ioutil.WriteFile(targetFile,[]byte(updatedContent),0644)
+}
+
+func startWatchingPath(path string) (chan time.Time, error) {
+
+	log.Debugf("Creating watcher for path %v", path)
 
 	// need a channel for calling back about changes happening in files
 	changeTime := make(chan time.Time)
 
 	// create a file watcher
-	watcher,err := fsnotify.NewWatcher()
-	if err !=nil {
-		log.Fatalf("Error creating file watcher: %v",err)
-		return nil,err
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatalf("Error creating file watcher: %v", err)
+		return nil, err
 	}
 
 	err = watcher.Add(path)
-	if os.IsNotExist(err){
+	if os.IsNotExist(err) {
 		log.Infof("%v no longer exists", path)
-	} else if err !=nil {
-		log.Warnf("Failed to watch %v because of error: %v",path,err)
+	} else if err != nil {
+		log.Warnf("Failed to watch %v because of error: %v", path, err)
 	}
 
-
 	// let the watcher run in the background
-	go listenForChanges(watcher,changeTime)
+	go listenForChanges(watcher, changeTime)
 
-	return changeTime,nil
+	return changeTime, nil
 }
 
-
-func listenForChanges(watcher *fsnotify.Watcher, changeTime chan time.Time){
+func listenForChanges(watcher *fsnotify.Watcher, changeTime chan time.Time) {
 
 	// main loop for processing events from the FS watcher
 	for {
 		select {
-			case err := <- watcher.Errors:
-				log.Errorf("Error from watcher: %v",err)
+		case err := <-watcher.Errors:
+			log.Errorf("Error from watcher: %v", err)
 
-				case event := <- watcher.Events:
-				log.Debugf("Received an event for %v",event.Name)
-				modifiedTime,err := getModifiedAtTime(event.Name)
-				if err != nil {
-					continue
-				}
+		case event := <-watcher.Events:
+			log.Debugf("Received an event for %v", event.Name)
+			stat, err := os.Stat(event.Name)
+			if err != nil {
+				log.Errorf("Could not get modified time for %v: %v", event.Name, err)
+				continue
+			}
+			log.Debugf("Modified time of %v is %v", event.Name, stat.ModTime())
 
-				changeTime <- modifiedTime
+			changeTime <- stat.ModTime()
 
 		}
 	}
 
-}
-
-func getModifiedAtTime(path string) (time.Time, error){
-	stat,err := os.Stat(path)
-	if err != nil {
-		log.Errorf("Could not get modified time for %v: %v",path,err)
-		return time.Time{},err
-	}
-	log.Debugf("Modified time of %v is %v",path,stat.ModTime())
-	return stat.ModTime(),nil
 }
