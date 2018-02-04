3de2c97f8916e11c9b2c1e661fb51d61fb5e2f09
Ken Haines <ken@kenhaines.net>
2018-02-04 12:04:54 -0800

    Add file watcher setup and tracking of file modified times.

--- main.go
+++ main.go
@@ -1,1 +1,118 @@
-package prom_config_watcher
+/*
+MIT License
+
+Copyright (c) 2018 Ken Haines
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+
+package main
+
+
+import (
+	log "github.com/sirupsen/logrus"
+	"flag"
+	"github.com/go-fsnotify/fsnotify"
+	"os"
+
+	"time"
+)
+
+
+var (
+	watchedPath =  flag.String("watch-path","/config","Path to be watched (default /config)")
+	expandVars = flag.Bool("expand-vars",true,"Expand $env variables found in files (default true).")
+	copyFiles = flag.Bool("copy-files",true,"Copy config files to destination (default true)")
+	targetPath = flag.String("target-path","/processed-config","Path to copy processed files to (default /processed-config)")
+	prometheusUrl = flag.String("prometheus-url","http://localhost:9090/-/reload", "Url to send a POST to prometheus for it to reload its config. (default http://localhost:9090/-/reload)")
+	debugLogs = flag.Bool("debug",false,"Enable debug log output")
+	)
+
+
+func main() {
+	log.SetLevel(log.InfoLevel)
+	log.Info("Prometheus Configuration Watcher")
+	log.Info("Github: https://github.com/khaines/prom-config-watcher")
+	flag.Parse()
+
+
+}
+
+
+func startWatchingPath(path string) (chan time.Time,error) {
+
+	log.Debugf("Creating watcher for path %v",path)
+
+	// need a channel for calling back about changes happening in files
+	changeTime := make(chan time.Time)
+
+	// create a file watcher
+	watcher,err := fsnotify.NewWatcher()
+	if err !=nil {
+		log.Fatalf("Error creating file watcher: %v",err)
+		return nil,err
+	}
+
+	err = watcher.Add(path)
+	if os.IsNotExist(err){
+		log.Infof("%v no longer exists", path)
+	} else if err !=nil {
+		log.Warnf("Failed to watch %v because of error: %v",path,err)
+	}
+
+
+	// let the watcher run in the background
+	go listenForChanges(watcher,changeTime)
+
+	return changeTime,nil
+}
+
+
+func listenForChanges(watcher *fsnotify.Watcher, changeTime chan time.Time){
+
+	// main loop for processing events from the FS watcher
+	for {
+		select {
+			case err := <- watcher.Errors:
+				log.Errorf("Error from watcher: %v",err)
+
+				case event := <- watcher.Events:
+				log.Debugf("Received an event for %v",event.Name)
+				modifiedTime,err := getModifiedAtTime(event.Name)
+				if err != nil {
+					continue
+				}
+
+				changeTime <- modifiedTime
+
+		}
+	}
+
+}
+
+func getModifiedAtTime(path string) (time.Time, error){
+	stat,err := os.Stat(path)
+	if err != nil {
+		log.Errorf("Could not get modified time for %v: %v",path,err)
+		return time.Time{},err
+	}
+	log.Debugf("Modified time of %v is %v",path,stat.ModTime())
+	return stat.ModTime(),nil
+}
